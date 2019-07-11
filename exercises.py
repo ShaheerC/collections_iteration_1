3c2060ca047cef1cc9286429ea9e24d3bbdd60c2
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-11 15:34:11 -0400

    exercise 0

---
+++ exercises.py
@@ -0,0 +1,38 @@
+#LISTS
+
+fav_colors = ['purple', 'black', 'grey']
+fam_age = [28, 34, 64, 68]
+coin_flip = ['heads', 'tails', 'tails', 'tails', 'heads']
+artists = ['tool', 'wutang', 'sigurros']
+
+#DICTIONARIES
+
+definitions = {
+    'python': 'a big snake',
+    'cat': 'fluffy friend',
+    'sheep': 'like a cloud'
+}
+fav_movies = {
+    'City of God': 2002,
+    'OldBoy': 2003,
+    'Interstellar': 2014
+}
+city_pop = {
+    'Toronto': 2930000,
+    'Boston': 685094,
+    'Dhaka': 8906000
+}
+friends = {
+    'Ricky': 29,
+    'Hirad': 28,
+    'Atindra': 31
+}
+
+print(fav_colors)
+print(fam_age)
+print(coin_flip)
+print(artists)
+print(definitions)
+print(fav_movies)
+print(city_pop)
+print(friends)
